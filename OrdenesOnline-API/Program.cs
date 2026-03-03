d96f89a5c50cb533d34c0a37ad7f22f5c2ef2c40
Fernando Manuel <fmurgueytio@seminariosab.com.pe>
2026-03-03 16:24:48 -0500

    Dominio
    Se agregó el dominio de seminario para que funcionara

--- OrdenesOnline-API/Program.cs
+++ OrdenesOnline-API/Program.cs
@@ -49,7 +49,10 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend", policy =>
     {
         policy
-            .WithOrigins("http://localhost:4200")
+            .WithOrigins(
+                "http://localhost:4200",
+                "https://ordenes.seminariosab.com.pe"
+            )
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
@@ -74,15 +77,14 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseHttpsRedirection();
+app.UseCors("AllowFrontend");
+
 app.UseAuthentication();
 app.UseAuthorization();
 
 app.UseSwagger();
 app.UseSwaggerUI();
-
-app.UseHttpsRedirection();
-
-app.UseCors("AllowFrontend");
 
 app.MapControllers();
 
